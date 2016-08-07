8ca8d799c65c50f6c992576863c1dda7396b5ee7
Alexander Levin <alevin.ru@gmail.com>
2016-08-07 20:28:49 +0300

    clean on serve

--- gulp/server.js
+++ gulp/server.js
@@ -3,11 +3,24 @@
 var path = require('path');
 var conf = require('./conf');
 var gulp = conf.gulp;
+var runSequence = require('run-sequence');
+
+var $ = conf.plugins;
 
 import browserSyncInit from './browserSyncInit';
 
-gulp.task('serve', ['watch'], function () {
-  browserSyncInit([path.join(conf.paths.tmp, '/serve'), conf.paths.src]);
+gulp.task('clean:tmp', function(){
+  return gulp.src(conf.paths.tmp, {read: false})
+    .pipe($.clean());
+});
+
+
+gulp.task('serve', function () {
+  runSequence.use(gulp)(
+    'clean:tmp',
+    'watch',
+    ()=> {browserSyncInit([path.join(conf.paths.tmp, '/serve'), conf.paths.src]);}
+  );
 });
 
 gulp.task('serve:dist', ['build'], function () {
