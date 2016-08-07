fbdf139d6295d1e25e2f84b5db9b0ddb3dda8d39
Alexander Levin <alevin.ru@gmail.com>
2016-08-07 04:29:52 +0300

    babel support

--- gulp/scripts.js
+++ gulp/scripts.js
@@ -3,6 +3,7 @@
 var path = require('path');
 var conf = require('./conf');
 var gulp = conf.gulp;
+var plugins = conf.plugins;
 
 var browserSync = require('browser-sync');
 
@@ -10,15 +11,30 @@ var $ = conf.plugins;
 
 
 gulp.task('scripts-reload', function() {
-  return buildScripts()
+  lintScripts();
+  return transpileScripts()
     .pipe(browserSync.stream());
 });
 
 gulp.task('scripts', function() {
-  return buildScripts();
+  lintScripts();
+  return transpileScripts();
 });
 
-function buildScripts() {
+function transpileScripts() {
+  return gulp.src([
+    path.join(conf.paths.src, '/app/**/*.js'),
+    path.join('!' + conf.paths.src, '/app/**/*.spec.js'),
+    path.join('!' + conf.paths.src, '/app/**/*.mock.js')
+  ])
+    .pipe(plugins.sourcemaps.init())
+    .pipe(plugins.babel())
+    .pipe(plugins.sourcemaps.write(), '.')
+    .pipe(gulp.dest(path.join(conf.paths.tmp, '/serve/app/')))
+
+}
+
+function lintScripts() {
   return gulp.src(path.join(conf.paths.src, '/app/**/*.js'))
     .pipe($.eslint())
     .pipe($.eslint.format())
