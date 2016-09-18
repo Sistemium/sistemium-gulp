81666a4b73f839fb458ab62ea262504188269690
Alexander Levin <alevin.ru@gmail.com>
2016-09-18 17:00:26 +0300

    rm extra 'use strict' with gulp-concat-util

--- src/lib_tasks/build.js
+++ src/lib_tasks/build.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const ngAnnotate = require('gulp-ng-annotate');
 const conf = require('../conf/gulp.conf');
 const angularFilesort = require('gulp-angular-filesort');
-const concat = require ('gulp-concat');
+const concat = require ('gulp-concat-util');
 const eslint = require('gulp-eslint');
 const babel = require('gulp-babel');
 
@@ -16,7 +16,10 @@ function finishBuild() {
     .pipe(babel())
     .pipe(angularFilesort()).on('error', conf.errorHandler('AngularFilesort'))
     .pipe(ngAnnotate())
-    .pipe(concat(conf.concat || 'index.js'))
+    .pipe(concat(conf.concat || 'index.js', {process: function(src) {
+      return (src.trim() + '\n').replace(/(^|\n)[ \t]*('use strict'|"use strict");?\s*/g, '$1');
+    }}))
+    .pipe(concat.header('\'use strict\';\n\n'))
     .pipe(gulp.dest(conf.path.dist()));
 
 }
