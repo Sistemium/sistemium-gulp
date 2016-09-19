b5af2aed8592da97e0ad9559590aebfa39007245
Alexander Levin <alevin.ru@gmail.com>
2016-09-19 10:35:15 +0300

    serve:dist fix

--- src/conf/browsersync-dist.conf.js
+++ src/conf/browsersync-dist.conf.js
@@ -1,12 +1,19 @@
 const conf = require('./gulp.conf');
+const _ = require('lodash');
 
 module.exports = function () {
-  return {
+  return _.defaultsDeep(conf.defaults.browserSync, {
     server: {
       baseDir: [
         conf.paths.dist
       ]
     },
-    open: false
-  };
+    open: false,
+    reloadOnRestart: true,
+    notify: false,
+    port: 3000,
+    ui: {
+      port: 3001
+    }
+  });
 };
